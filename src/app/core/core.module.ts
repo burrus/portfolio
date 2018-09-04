8e38af9b3e937d991226737780441b2ca934a78c
Tony Burrus <tony@burrus.me>
2018-09-04 16:07:50 -0700

    added core header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent]
 })
 export class CoreModule { }
