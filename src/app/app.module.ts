b456977139ccfaaa892d6928a9eb9a6672e8d200
Tony Burrus <tony@burrus.me>
2018-09-04 15:36:13 -0700

    added work module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AboutModule } from './modules/about/about.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { WorkModule } from './modules/work/work.module';
 
 
 @NgModule({
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AboutModule,
+    BrowserModule,
+    CoreModule,
+    WorkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
