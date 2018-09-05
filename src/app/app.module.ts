45596d0ab3613bcfba19e4c86e50af21fb189caa
Tony Burrus <tony@burrus.me>
2018-09-05 15:34:40 -0700

    installed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 
 import { AboutModule } from './modules/about/about.module';
@@ -16,6 +17,7 @@ import { WorkModule } from './modules/work/work.module';
     AppRouting,
     BrowserModule,
     CoreModule,
+    NgbModule.forRoot(),
     WorkModule
   ],
   providers: [],
