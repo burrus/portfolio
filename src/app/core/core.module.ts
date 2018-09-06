facee2b03d79f339aa3859aa67c6eca9c6e78662
Tony Burrus <tony@burrus.me>
2018-09-06 13:52:03 -0700

    imported ng-bootsrap into core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
 
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   declarations: [FooterComponent, HeaderComponent],
   exports: [FooterComponent, HeaderComponent]
 })
-export class CoreModule { }
+export class CoreModule {}
