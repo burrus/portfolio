ede7037ffa4e3c1470b57e9c3e13c6b2282f1798
Tony Burrus <tony@burrus.me>
2018-09-11 17:08:37 -0700

    added instagram component

---
+++ src/app/shared/components/instagram/instagram.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'instagram',
+  templateUrl: './instagram.component.html',
+  styleUrls: ['./instagram.component.css']
+})
+export class InstagramComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { InstagramComponent } from './components/instagram/instagram.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [InstagramComponent]
 })
 export class SharedModule { }
